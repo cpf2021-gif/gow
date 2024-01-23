9a55d32794d50fb6620ace26f0967a70ec0eba5f
xypf <2992247892@qq.com>
2024-01-23 11:27:34 +0800

    feat(example): Update for new version
    Update the example code to be compatible with the latest version

--- test/example.go
+++ test/example.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/cpf2021-gif/gow"
@@ -9,14 +8,20 @@ import (
 
 func main() {
 	r := gow.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+
+	r.GET("/", func(c *gow.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello gow</h1>")
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/hello", func(c *gow.Context) {
+		c.String(http.StatusOK, "hello, Have a nice day!\n")
+	})
+
+	r.POST("/login", func(c *gow.Context) {
+		c.JSON(http.StatusOK, gow.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":9999")
